a912b7d86704baa70abead09105ade99114bc9b0
EmileLevast <emilelevast@outlook.fr>
2020-10-06 15:58:36 +0200

    feat: Deplacement simple
    - Ajoute une tete qui bouge simplement selon les fleches
    - Ajoute une scene principale et detruit la sample scene
    - Ajoute un background

---
+++ Assets/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotationSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translationY = Input.GetAxis("Vertical") * speed;
+        float translationX = Input.GetAxis("Horizontal") * speed;
+
+        translationY *= Time.deltaTime;
+        translationX *= Time.deltaTime;
+
+        transform.Translate(translationX, translationY, 0f);
+
+        //transform.Rotate(0, rotation, 0);
+    }
+}
