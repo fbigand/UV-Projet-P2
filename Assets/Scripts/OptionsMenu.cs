b7499d9aa40aeacae30e2bde797aadbb2f0c5e03
Franck Bigand <fbigand@etu.imt-lille-douai.fr>
2020-10-07 09:26:50 +0200

    fix audio ; put scripts to script folder

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+
 public class OptionsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     
     public void SetVolume(float volume)
     {
+        Debug.Log(volume);
         audioMixer.SetFloat("volume", volume);
     }
 }
