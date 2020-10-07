b8f8065417bea37f5adb109d2f3ccded633fd90f
EmileLevast <emilelevast@outlook.fr>
2020-10-07 10:26:56 +0200

    Deplacement continu avec rotation

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float speed = 1f;
-    public float rotationSpeed = 1f;
+    public float speed = 0.01f;
+    public float rotationAngle = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,24 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float translationY = speed;
+
+        float rotationZ = Input.GetAxis("Horizontal");
+
+        //The player want to turn
+        if(rotationZ != 0)
+        {
+            //On inverse le signe pour avoir un sens de rotation cohérent
+            rotationZ = -1f * Mathf.Sign(rotationZ) ;
+        }
+
+        transform.Rotate(0f, 0f, rotationZ*rotationAngle);
+        transform.Translate(0f, translationY, 0f);
+    }
+
+    // TODO: Delete this function to keep movement according to rotation instead
+    private void simpleMoveWithArrows()
+    {
         float translationY = Input.GetAxis("Vertical") * speed;
         float translationX = Input.GetAxis("Horizontal") * speed;
 
@@ -22,7 +40,5 @@ public class movement : MonoBehaviour
         translationX *= Time.deltaTime;
 
         transform.Translate(translationX, translationY, 0f);
-
-        //transform.Rotate(0, rotation, 0);
     }
 }
