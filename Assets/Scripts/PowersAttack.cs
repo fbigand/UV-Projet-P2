c55254cfb51acbebec098d0975960633cd71fc1e
Alex Gautier <axgautier83@gmail.com>
2020-10-08 15:29:06 +0200

    Début Rocket + Bouton Restart

---
+++ Assets/Scripts/PowersAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowersAttack : MonoBehaviour
+{ 
+    //Point de tir
+    public Transform shootPoint;
+
+    // Floats
+    public float rocketSpeed = 3f;
+
+    //Objets à faire spawn
+    public GameObject rocketPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) == true)
+        {
+            SimpleRocket(true);
+        }
+    }
+
+    void SimpleRocket(bool attacking)
+    {
+        if (attacking)
+        {
+            GameObject rocket = Instantiate(rocketPrefab, shootPoint.transform.position, shootPoint.transform.rotation) as GameObject;
+            rocket.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.position.x,transform.position.y) * rocketSpeed;
+        }
+        
+    }
+
+}
