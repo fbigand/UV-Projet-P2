bd17c3716206f24b469ac10d4c311c4a944c0fb8
Franck Bigand <fbigand@etu.imt-lille-douai.fr>
2020-10-16 14:33:24 +0200

    add predefined trajectory controller ; ship can loop in spiral

---
+++ Assets/Scripts/ShipController/ControllerPredefined.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ControllerPredefined : MonoBehaviour, IController
+{
+    private Vector3 startPosition;
+    private Stopwatch time = new Stopwatch();
+    private double rotationFactor = 3680; // empirical value
+
+    private void Start()
+    {
+        startPosition = transform.position;
+        time.Start();
+    }
+
+    float IController.GetRotation()
+    {
+        Vector3 currentPosition = transform.position;
+        float distanceFromStart = Vector3.Distance(startPosition, currentPosition);
+        double maxDistance = math.sqrt(time.Elapsed.TotalMilliseconds / rotationFactor);
+
+        return distanceFromStart > maxDistance ? -1f : 0f;
+    }
+
+    bool IController.IsUsingPrimaryBonus()
+    {
+        return false;
+    }
+
+    bool IController.IsUsingSecondaryBonus()
+    {
+        return false;
+    }
+}
