55a7d29b83ffbc413e8d750570dcc616304b4c0b
Alex Gautier <axgautier83@gmail.com>
2020-10-19 16:28:55 +0200

    Modif PickUps Superclass

--- Assets/Scripts/PickUps/PickUps.cs
+++ Assets/Scripts/PickUps/PickUps.cs
@@ -6,5 +6,12 @@ public abstract class PickUps : MonoBehaviour
 {
     public float speedModif = 0.005f;
 
+    public GameObject[] activePlayers;
+
+    private void Awake()
+    {
+        activePlayers = GameObject.FindGameObjectWithTag("GameManager").GetComponent<ManagePlayers>().activePlayers;
+    }
+
     public abstract void ActivatePickUp(GameObject ship);
 }
