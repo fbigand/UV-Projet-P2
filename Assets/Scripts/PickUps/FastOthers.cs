c9ee5390165ef9991f628cea47cd5752624822f7
Alex Gautier <axgautier83@gmail.com>
2020-10-19 19:27:16 +0200

    Add comestibles Others

--- Assets/Scripts/PickUps/FastOthers.cs
+++ Assets/Scripts/PickUps/FastOthers.cs
@@ -7,14 +7,24 @@ public class FastOthers : PickUps
     override public void ActivatePickUp(GameObject ship)
     {
         gameObject.transform.position = new Vector3(transform.position.x, transform.position.y, 1);
-        ship.GetComponent<ShipMovement>().speed += speedModif;
+
+        foreach (var head in activePlayers)
+        {
+            head.GetComponent<ShipMovement>().speed += speedModif;
+        }
+
+        ship.GetComponent<ShipMovement>().speed -= speedModif;
         StartCoroutine(WaitAndReset(ship.GetComponent<ShipMovement>()));
     }
 
     IEnumerator WaitAndReset(ShipMovement shipmov)
     {
         yield return new WaitForSeconds(5);
-        shipmov.speed -= speedModif;
+        foreach (var head in activePlayers)
+        {
+            head.GetComponent<ShipMovement>().speed -= speedModif;
+        }
+        shipmov.speed += speedModif;
         Destroy(gameObject);
     }
 }
