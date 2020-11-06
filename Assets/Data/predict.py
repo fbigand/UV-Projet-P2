e220176b4b3fe931eaf1170919e9a3eb124fe419
EmileLevast <emilelevast@outlook.fr>
2020-11-06 14:34:26 +0100

    Python script execute correctly

--- Assets/Data/predict.py
+++ Assets/Data/predict.py
@@ -5,28 +5,34 @@ import numpy as np
 #!/usr/bin/python
 
 def main(argv):
-  
-  print("1");
-  pkl_filename = "pickle_model.pkl"
+
+  #print("1")
+  pkl_filename = "C:\\Users\\emile\\Documents\\unity\\UV-Projet-P2\\Assets\\Data\\pickle_model.pkl"
   
   with open(pkl_filename, 'rb') as file:
     pickle_model = pickle.load(file)
 
-  #xMin = -1.2
-  #xMax = 9.6
-  #yMin = -5.4
-  #yMax = 5.4
+  xMin = -1.2
+  xMax = 9.6
+  yMin = -5.4
+  yMax = 5.4
 
-  #argv[0] = (argv[0] + 1.2)/10.8
-  #argv[1] = (argv[1]+5.4)/10.8
-  #argv[2] = argv[2]/360
+  array_of_string = argv[0].split(";")
+  string_to_predict = np.asarray(array_of_string)
+  data_to_predict = string_to_predict.astype(np.float)
 
-  #ligne_vide = np.zeros(len(argv))
-  #input = [argv,ligne_vide]
+  data_to_predict[0] = (data_to_predict[0] + 1.2)/10.8
+  data_to_predict[1] = (data_to_predict[1]+5.4)/10.8
+  data_to_predict[2] = data_to_predict[2]/360
 
-  #output = pickle_model.predict(input)
+  ligne_vide = np.zeros(len(data_to_predict))
+  input = [data_to_predict,ligne_vide]
 
-  #print(output[0])
+  output = pickle_model.predict(input)
+
+  print(output[0])
+
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
